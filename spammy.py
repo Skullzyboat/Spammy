a4ea2baf7327028f6cd06311c1067bc4505adfc0
Skullzyboat <60168149+Skullzyboat@users.noreply.github.com>
2020-08-21 09:14:18 -0500

    Update spammy.py

--- spammy.py
+++ spammy.py
@@ -1,10 +1,23 @@
-import pyautogui
-import time
+import pyautogui, time
+print("\u001b[31mWARNING!!!")
+time.sleep(1)
+print("\u001b[31mWARNING!!!")
+time.sleep(1)
+print("\u001b[31mWARNING!!!")
+
+time.sleep(1)
+
+print("\u001b[31mDO NOT ABUSE AND OR USE WITHOUT CONSENT OR FOR HARASSMENT")
+
+2
+time.sleep(2)
 
 msg = input("\u001b[33mEnter Your message: ")
 n = input("\u001b[33mHow many times would you like it to be spammed? ?: ")
 
 print("\u001b[34mstarting in:")
+print("\u001b[36m")
+time.sleep(1)
 
 count = 5
 while(count != 0):
@@ -14,5 +27,7 @@ while(count != 0):
 
 print("\u001b[31mFire in the hole!!!")
 
+time.sleep(1)
+
 for i in range(0,int(n)):
-	pyautogui.typewrite(msg + '\n')
+   	pyautogui.typewrite(msg + '\n')
